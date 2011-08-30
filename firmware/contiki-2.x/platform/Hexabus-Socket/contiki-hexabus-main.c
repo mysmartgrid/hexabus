417b2fcf3ba33fdb91a7913b3d822a76ad06199e
Mathias Dalheimer <md@gonium.net>
2011-08-30 10:40:17 +0200

    added ds18S20 library routines & process

--- firmware/contiki-2.x/platform/Hexabus-Socket/contiki-hexabus-main.c
+++ firmware/contiki-2.x/platform/Hexabus-Socket/contiki-hexabus-main.c
@@ -100,6 +100,7 @@
 //HEXABUS includes
 #include "button.h"
 #include "metering.h"
+#include "temperature.h"
 #include "relay.h"
 #include "eeprom_variables.h"
 #include "udp_handler.h"
@@ -305,6 +306,9 @@ void initialize(void)
 
   /* Init Metering */
   metering_init();
+
+  /* Init Temp Sensor */
+  temperature_init();
 
   /*Init Relay */
   relay_init();
