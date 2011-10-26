0467b214e7f115d9809576b5a856822e1ac5a190
Bernd Lietzow <github@ebrnd.de>
2011-10-26 14:32:04 +0200

    State machine core

--- statemaschine_test/statemaschine.cpp
+++ statemaschine_test/statemaschine.cpp
@@ -9,7 +9,7 @@ bool eval(Condition *cond, uint8_t ip, uint8_t eid, uint8_t value) { 		// FIXME:
 		//cout << "Nope, Chuck Testa." << endl;
 		return false;
 	}
-	// Great, now we have to do actual work
+
 	switch(cond->op) {
 		case eq: 
 			cout << "Check for ==" << endl;
@@ -31,9 +31,9 @@ bool eval(Condition *cond, uint8_t ip, uint8_t eid, uint8_t value) { 		// FIXME:
 	}
 }
 
-int testfunction(int i)
+int testfunction(uint8_t i)
 {
-  return 1;
+  return 0;
 }
 
 int main() {
@@ -57,36 +57,46 @@ int main() {
 	onToOff.action = 0;
 	onToOff.toCool = 0;
 	onToOff.toUncool = 1;
-	
+
 	Transition table[2] = {offToOn, onToOff};
 	uint8_t curState;
 	uint8_t ip, eid, value;
 	int tmp;
-	cout << "Enter IP (uint8):";
-	cin >> tmp;
-	ip = tmp;
-	cout << "Enter EID (uint8)";
-	cin >> tmp;
-	eid = tmp;
-	cout << "Enter value (uint8)";
-	cin >> tmp;
-	value = tmp;
-	
-	// Search for the current state and check for matching conditions
-	for(uint8_t i = 0;i < (sizeof(table)/sizeof(Transition));i++) {
-		if((table[i].fromState == curState) && (eval(table[i].cond, ip, eid, value))) {
-			// Found match
-			cout << "Executing function number " << (int)table[i].action << endl;
-			if(testfunction((int)table[i].action) == 0)
+
+  // So now let's do an actual state machine
+  curState = 0; // start out in State 0.
+  while(true)
+  {
+    // ask user for input
+    cout << "Enter IP (uint8):";
+    cin >> tmp;
+    ip = tmp;
+    cout << "Enter EID (uint8)";
+    cin >> tmp;
+    eid = tmp;
+    cout << "Enter value (uint8)";
+    cin >> tmp;
+    value = tmp;
+
+    for(uint8_t i = 0;i < (sizeof(table)/sizeof(Transition));i++)
+    {
+      if((table[i].fromState == curState) && (eval(table[i].cond, ip, eid, value)))
       {
-			  cout << "New State = Cool State = " << (int)table[i].toCool << endl;
-      } else {
-        cout << "New State = Uncool State = " << (int)table[i].toUncool << endl;
+        // Found match
+        cout << "Executing function number " << (int)table[i].action << endl;
+        if(testfunction(table[i].action) == 0)
+        {
+          cout << "New State = Cool State = " << (int)table[i].toCool << endl;
+          curState = table[i].toCool;
+          break;
+        } else {
+          cout << "New State = Uncool State = " << (int)table[i].toUncool << endl;
+          curState = table[i].toUncool;
+          break;
+        }
       }
-			return 0;
-		}
-	}
-	cout << "No match found" << endl;
+    }
+  }
 	return 0;
 }
 
