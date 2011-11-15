d979555e8278320f06c0793fd6db5a7c164c31a2
Dominik Keller <kellerd@itwm.fhg.de>
2011-11-15 11:07:23 +0100

    cleaned up octave script

--- hostsoftware/hexaswitch/tools/temperature.m
+++ hostsoftware/hexaswitch/tools/temperature.m
@@ -3,7 +3,7 @@
 
 # load the files containing time and temperature values
 # first column is timestamp, second column is temperature value
-temp1=load("-ascii", 'fe80::2ce:4200:70:428f%usb0')
+temp1=load("-ascii", 'fe80::2ce:4200:70:428f%usb0');
 temp2=load("-ascii", 'fe80::50:c4ff:fe04:8010%usb0'); % kappoth
 temp3=load("-ascii", 'fe80::50:c4ff:ff04:8005%usb0'); % hat offset +1°C
 temp4=load("-ascii", 'fe80::50:c4ff:fe04:8009%usb0');
@@ -11,17 +11,18 @@ temp4=load("-ascii", 'fe80::50:c4ff:fe04:8009%usb0');
 %temp1_time=[];
 
 clf;
-figure(1);
 plot(temp1(:,1),temp1(:,2), "-b;Sensor 1;");
 hold on;
-plot(temp2(:,1),temp2(:,2), "-r;Sensor 2;");
-plot(temp3(:,1),temp3(:,2), "-g;Sensor 3;");
-plot(temp4(:,1),temp4(:,2), "-k;Sensor 4;");
+# Sensor 2 is way off, so don't plot it.
+#plot(temp2(:,1),temp2(:,2)-15.5, "-k;Sensor 2;");
+# Sensor 3 has a offset of 1K, so correct this when plotting.
+plot(temp3(:,1),temp3(:,2)-1, "-g;Sensor 3;");
+plot(temp4(:,1),temp4(:,2), "-r;Sensor 4;");
 hold off;
 
 # Add descriptions 
 grid;
-xlabel('t [s]');
+xlabel('time');
 ylabel('\vartheta [°C]');
 title('Temperature over time');
 time_labels=get(gca(), "xtick");
@@ -36,4 +37,4 @@ set(gca(), "xticklabel", time_reallabels);
 # ylim([23.5 26]);
 
 # save plot as a PDF file
-#print('temperature.pdf','-dpdf')
+print('temperature.png','-dpng')
