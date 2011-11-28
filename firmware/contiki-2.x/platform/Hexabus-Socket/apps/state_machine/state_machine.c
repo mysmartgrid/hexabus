24a7fe7a32068e81455a170aee6378fa7b45c094
Bernd Lietzow <github@ebrnd.de>
2011-11-28 13:11:51 +0100

    some formatting

--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.c
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.c
@@ -6,7 +6,7 @@
 #include "endpoint_access.h"
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>			// memcpy
+#include <string.h>      // memcpy
 
 // #if STATE_MACHINE_DEBUG 
 #include <stdio.h>
@@ -25,143 +25,144 @@ AUTOSTART_PROCESSES(&state_machine_process);
 /*------------------------------------------------------*/
 
 static struct condition *condTable;
-static struct transition *transTable;		
-static uint8_t transLength = 2;							// length of transition table
-static uint8_t curState = 0;						// starting out in state 0
+static struct transition *transTable;
+static uint8_t transLength = 2;              // length of transition table
+static uint8_t curState = 0;            // starting out in state 0
 
 bool eval(uint8_t condIndex, struct hxb_data *data) {
-	struct condition *cond = &condTable[condIndex];
-	
-	// Check IPs and EID
-	bool null = true;
-	bool equal = true;
-	uint8_t k = 0;
-	for(k = 0;k < 16;k++) {
-		if(cond->sourceIP[k] != 0) {
-			null = false;
-		}
-		if(cond->sourceIP[k] != data->source[k]) {
-			equal = false;
-		}
-	}
-	
-	if((!null && !equal) || (cond->sourceEID != data->eid)) {
-		return false;
-	}
-	
-	// Now check for the actual data. first of all, we have to check for compatible datatypes
-	if(data->value.datatype != cond->value.datatype) {
-		return false;
-	}
+  struct condition *cond = &condTable[condIndex];
 
-	// Now lets check for the operator and then for datatypes. TODO: Datetime comperator
-	switch(cond->op) {
-		case eq:
-			if((cond->value.datatype == HXB_DTYPE_UINT8) || (cond->value.datatype == HXB_DTYPE_BOOL))
-				return (cond->value.int8 == data->value.int8);
-			if(cond->value.datatype == HXB_DTYPE_UINT32)
-				return (cond->value.int32 == data->value.int32);
-		case leq:
-			if(cond->value.datatype == HXB_DTYPE_UINT8)
-				return (cond->value.int8 <= data->value.int8);
-			if(cond->value.datatype == HXB_DTYPE_UINT32)
-				return (cond->value.int32 <= data->value.int32);
-		case geq:
-			if(cond->value.datatype == HXB_DTYPE_UINT8)
-				return (cond->value.int8 >= data->value.int8);
-			if(cond->value.datatype == HXB_DTYPE_UINT32)
-				return (cond->value.int32 >= data->value.int32);
-		case lt:
-			if(cond->value.datatype == HXB_DTYPE_UINT8)
-				return (cond->value.int8 < data->value.int8);
-			if(cond->value.datatype == HXB_DTYPE_UINT32)
-				return (cond->value.int32 < data->value.int32);
-		case gt:
-			if(cond->value.datatype == HXB_DTYPE_UINT8)
-				return (cond->value.int8 > data->value.int8);
-			if(cond->value.datatype == HXB_DTYPE_UINT32)
-				return (cond->value.int32 > data->value.int32);
-		default:
-			return false;
-	}
+  // Check IPs and EID
+  bool null = true;
+  bool equal = true;
+  uint8_t k = 0;
+  for(k = 0;k < 16;k++) {
+    if(cond->sourceIP[k] != 0) {
+      null = false;
+    }
+    if(cond->sourceIP[k] != data->source[k]) {
+      equal = false;
+    }
+  }
+
+  if((!null && !equal) || (cond->sourceEID != data->eid)) {
+    return false;
+  }
+
+  // Now check for the actual data. first of all, we have to check for compatible datatypes
+  if(data->value.datatype != cond->value.datatype) {
+    return false;
+  }
+
+  // Now lets check for the operator and then for datatypes. TODO: Datetime comperator
+  switch(cond->op) {
+    case eq:
+      if((cond->value.datatype == HXB_DTYPE_UINT8) || (cond->value.datatype == HXB_DTYPE_BOOL))
+        return (cond->value.int8 == data->value.int8);
+      if(cond->value.datatype == HXB_DTYPE_UINT32)
+        return (cond->value.int32 == data->value.int32);
+    case leq:
+      if(cond->value.datatype == HXB_DTYPE_UINT8)
+        return (cond->value.int8 <= data->value.int8);
+      if(cond->value.datatype == HXB_DTYPE_UINT32)
+        return (cond->value.int32 <= data->value.int32);
+    case geq:
+      if(cond->value.datatype == HXB_DTYPE_UINT8)
+        return (cond->value.int8 >= data->value.int8);
+      if(cond->value.datatype == HXB_DTYPE_UINT32)
+        return (cond->value.int32 >= data->value.int32);
+    case lt:
+      if(cond->value.datatype == HXB_DTYPE_UINT8)
+        return (cond->value.int8 < data->value.int8);
+      if(cond->value.datatype == HXB_DTYPE_UINT32)
+        return (cond->value.int32 < data->value.int32);
+    case gt:
+      if(cond->value.datatype == HXB_DTYPE_UINT8)
+        return (cond->value.int8 > data->value.int8);
+      if(cond->value.datatype == HXB_DTYPE_UINT32)
+        return (cond->value.int32 > data->value.int32);
+    default:
+      return false;
+  }
 }
 
 PROCESS_THREAD(state_machine_process, ev, data)
 {
-	  
-	PROCESS_BEGIN();
+  PROCESS_BEGIN();
+
   PRINTF("State Machine starting.\r\n");
-	/* Definition of an actual state machine. Simple relay-Trigger */
-	condTable = malloc(sizeof(struct condition));
-	transTable = malloc(transLength*sizeof(struct transition));
-	struct hxb_value wData;
-	wData.datatype = HXB_DTYPE_BOOL;
-	wData.int8 = HXB_FALSE;
+  /* Definition of an actual state machine. Simple relay-Trigger */
+  condTable = malloc(sizeof(struct condition));
+  transTable = malloc(transLength*sizeof(struct transition));
+  struct hxb_value wData;
+  wData.datatype = HXB_DTYPE_BOOL;
+  wData.int8 = HXB_FALSE;
 
-	uint8_t a = 0;
-	for(a = 0;a < 16;a++) {
-		condTable[0].sourceIP[a] = 0;
-	}
-	condTable[0].sourceEID = 1;
-	condTable[0].op = eq;
-	condTable[0].value.datatype = HXB_DTYPE_UINT8;
-	condTable[0].value.int8 = 10;
+  uint8_t a = 0;
+  for(a = 0;a < 16;a++) {
+    condTable[0].sourceIP[a] = 0;
+  }
 
-	transTable[0].fromState = 0;
-	transTable[0].cond = 0;
-	transTable[0].eid = 1;
-	memcpy(&(transTable[0].data), &wData, sizeof(struct hxb_value));
-	transTable[0].goodState = 1;
-	transTable[0].badState = 0;
-	transTable[1].fromState = 1;
-	transTable[1].cond = 0;
-	transTable[1].eid = 1;
-	wData.int8 = HXB_TRUE;
-	memcpy(&(transTable[1].data), &wData, sizeof(struct hxb_value));
-	transTable[1].goodState = 0;
-	transTable[1].badState = 1;
-	/*----------------------------------------*/
+  condTable[0].sourceEID = 1;
+  condTable[0].op = eq;
+  condTable[0].value.datatype = HXB_DTYPE_UINT8;
+  condTable[0].value.int8 = 10;
+
+  transTable[0].fromState = 0;
+  transTable[0].cond = 0;
+  transTable[0].eid = 1;
+  memcpy(&(transTable[0].data), &wData, sizeof(struct hxb_value));
+  transTable[0].goodState = 1;
+  transTable[0].badState = 0;
+  transTable[1].fromState = 1;
+  transTable[1].cond = 0;
+  transTable[1].eid = 1;
+  wData.int8 = HXB_TRUE;
+  memcpy(&(transTable[1].data), &wData, sizeof(struct hxb_value));
+  transTable[1].goodState = 0;
+  transTable[1].badState = 1;
+  /*----------------------------------------*/
 
   /* DEBUG */
-		int k = 0;
-		PRINTF("[State Machine Table]: From | Cond | EID | Good | Bad\r\n");
-		for(k = 0;k < transLength;k++) {
-			PRINTF(" %d | %d | %d | %d | %d \r\n", transTable[k].fromState, transTable[k].cond, transTable[k].eid, transTable[k].goodState, transTable[k].badState);
-		}
-	/* END */
-	
-	while(1)
+    int k = 0;
+    PRINTF("[State Machine Table]: From | Cond | EID | Good | Bad\r\n");
+    for(k = 0;k < transLength;k++) {
+      PRINTF(" %d | %d | %d | %d | %d \r\n", transTable[k].fromState, transTable[k].cond, transTable[k].eid, transTable[k].goodState, transTable[k].badState);
+    }
+  /* END */
+
+  while(1)
   {
     PROCESS_WAIT_EVENT();
-  	if(ev == sm_data_received_event)
+    if(ev == sm_data_received_event)
     {
       PRINTF("State machine: Received event\r\n");
-			// something happened, better check our own tables
-			struct hxb_data *edata = (struct hxb_data*)data;
-			uint8_t i;
-			for(i = 0;i < transLength;i++)
+      // something happened, better check our own tables
+      struct hxb_data *edata = (struct hxb_data*)data;
+      uint8_t i;
+      for(i = 0;i < transLength;i++)
       {
-				if((transTable[i].fromState == curState) && (eval(transTable[i].cond, edata)))
+        if((transTable[i].fromState == curState) && (eval(transTable[i].cond, edata)))
         {
-					// Match found
-					PRINTF("state_machine: Writing to endpoint %d \r\n", transTable[i].eid);
-					if(endpoint_write(transTable[i].eid, &(transTable[i].data)) == 0)
-          {			
-						curState = transTable[i].goodState;
-						PRINTF("state_machine: Everything is fine \r\n");
-						break;
-					} else {													// Something went wrong
-						curState = transTable[i].badState;
-						PRINTF("state_machine: Something bad happened \r\n");
-						break;
-					}
-				}
-			}
+          // Match found
+          PRINTF("state_machine: Writing to endpoint %d \r\n", transTable[i].eid);
+          if(endpoint_write(transTable[i].eid, &(transTable[i].data)) == 0)
+          {      
+            curState = transTable[i].goodState;
+            PRINTF("state_machine: Everything is fine \r\n");
+            break;
+          } else {                          // Something went wrong
+            curState = transTable[i].badState;
+            PRINTF("state_machine: Something bad happened \r\n");
+            break;
+          }
+        }
+      }
 
       free(data);
-		}
-	}
+    }
+  }
 
-	PROCESS_END();
+  PROCESS_END();
 }
 
--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.h
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.h
@@ -14,22 +14,22 @@ PROCESS_NAME(state_machine_process);
 // Internal stuff: these structs implement a simple table layout
 
 // Operators for comparison
-enum oper { eq, leq, geq, lt, gt};
+enum oper { eq, leq, geq, lt, gt };
 
 struct condition {
-	char sourceIP[16];		// TODO: IP
-	uint8_t sourceEID;	// EID we expect data from
-	enum oper op;				// predicate function
-	struct hxb_value value;			// the constant to compare with
+  char sourceIP[16];      // TODO: IP
+  uint8_t sourceEID;      // EID we expect data from
+  enum oper op;           // predicate function
+  struct hxb_value value; // the constant to compare with
 };
 
 struct transition {
-	uint8_t fromState;					// current state
-	uint8_t cond;								// index of condition that must be matched
-	uint8_t eid;							// id of endpoint which should do something
-	uint8_t goodState;					// new state if everything went fine
-	uint8_t badState;						// new state if some went wrong
-	struct hxb_value data;			// Data for the endpoint
+  uint8_t fromState;      // current state
+  uint8_t cond;           // index of condition that must be matched
+  uint8_t eid;            // id of endpoint which should do something
+  uint8_t goodState;      // new state if everything went fine
+  uint8_t badState;       // new state if some went wrong
+  struct hxb_value data;  // Data for the endpoint
 };
 
 
