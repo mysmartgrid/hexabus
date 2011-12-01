efb7f6433fda2e6f2eb844ee6056f62587d167c3
Bernd Lietzow <github@ebrnd.de>
2011-12-01 15:29:14 +0100

    debug #defines

--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.c
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/state_machine/state_machine.c
@@ -10,16 +10,16 @@
 #include <stdlib.h>
 #include <string.h>      // memcpy
 
-// #if STATE_MACHINE_DEBUG 
+#if STATE_MACHINE_DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
 #define PRINT6ADDR(addr) PRINTF(" %02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x ", ((u8_t *)addr)[0], ((u8_t *)addr)[1], ((u8_t *)addr)[2], ((u8_t *)addr)[3], ((u8_t *)addr)[4], ((u8_t *)addr)[5], ((u8_t *)addr)[6], ((u8_t *)addr)[7], ((u8_t *)addr)[8], ((u8_t *)addr)[9], ((u8_t *)addr)[10], ((u8_t *)addr)[11], ((u8_t *)addr)[12], ((u8_t *)addr)[13], ((u8_t *)addr)[14], ((u8_t *)addr)[15])
 #define PRINTLLADDR(lladdr) PRINTF(" %02x:%02x:%02x:%02x:%02x:%02x ",(lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3],(lladdr)->addr[4], (lladdr)->addr[5])
-// #else
-// #define PRINTF(...)
-// #define PRINT6ADDR(addr)
-// #define PRINTLLADDR(addr)
-// #endif
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
 
 /*------------------------------------------------------*/
 PROCESS(state_machine_process, "State Machine Process");
@@ -274,6 +274,7 @@ PROCESS_THREAD(state_machine_process, ev, data)
   transLength = eeprom_read_byte((void*)EE_STATEMACHINE_TRANSITIONS); 
   dtTransLength = eeprom_read_byte((void*)EE_STATEMACHINE_DATETIME_TRANSITIONS);
 
+#if STATE_MACHINE_DEBUG
   // output tables so we see if reading it works
   struct transition* tt = malloc(sizeof(struct transition));
   if(tt != NULL)
@@ -295,8 +296,9 @@ PROCESS_THREAD(state_machine_process, ev, data)
     PRINTF("malloc failed!\r\n");
   }
   free(tt);
+#endif // STATE_MACHINE_DEBUG
 
-  // initialize timers
+  // initialize timer
   static struct etimer check_timer;
   etimer_set(&check_timer, CLOCK_SECOND * 5); // TODO do we want this configurable?
 
--- firmware/contiki-2.x/platform/Hexabus-Socket/hexabus_config.h
+++ firmware/contiki-2.x/platform/Hexabus-Socket/hexabus_config.h
@@ -26,6 +26,6 @@
 
 // state_machine
 #define STATE_MACHINE_ENABLE 1
-#define STATE_MACHINE_DEBUG 1
+#define STATE_MACHINE_DEBUG 0
 
 #endif // HEXBAUS_CONFIG_H
