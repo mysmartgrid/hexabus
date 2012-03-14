69bae5b8ba03db6c8571ebb93af28834de2e93da
Simon Birnbach <simon@simon-birnbach.de>
2012-03-14 15:40:05 +0100

    Update datetime on press/release event

--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/treppenlichtdemo/treppenlichtdemo.c
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/treppenlichtdemo/treppenlichtdemo.c
@@ -6,40 +6,42 @@
 
 #include "hexabus_config.h"
 
-static uint8_t is_day = TREPPENHAUSLICHT_IS_DAY;
+//static uint8_t is_day = TREPPENHAUSLICHT_IS_DAY;
 static struct datetime daytime;
 static struct datetime nighttime;
 
-daytime->hour = 12;
-daytime->minute = 0;
-daytime->second = 0;
-daytime->day = 1;
-daytime->month = 1;
-daytime->year = 1970;
-daytime->weekday = 4;
-
-nighttime->hour = 23;
-nighttime->minute = 0;
-nighttime->second = 0;
-nighttime->day = 1;
-nighttime->month = 1;
-nighttime->year = 1970;
-nighttime->weekday = 4;
-
-struct hxb_value val;
-
-PROCESS_THREAD(state_machine_process, ev, data)
+PROCESS_THREAD(treppenlicht_demo_process, ev, data)
 {
 	PROCESS_BEGIN();
+
+	daytime.hour = 12;
+	daytime.minute = 0;
+	daytime.second = 0;
+	daytime.day = 1;
+	daytime.month = 1;
+	daytime.year = 1970;
+	daytime.weekday = 4;
+
+	nighttime.hour = 23;
+	nighttime.minute = 0;
+	nighttime.second = 0;
+	nighttime.day = 1;
+	nighttime.month = 1;
+	nighttime.year = 1970;
+	nighttime.weekday = 4;
+
+	struct hxb_envelope* envelope = malloc(sizeof(struct hxb_envelope));
 	while(1) {
 		PROCESS_WAIT_EVENT();
 
 
 		if(ev == demo_licht_pressed_event) {
-			updateDatetime(daytime);
+			memcpy(&(envelope->value.data), &(daytime), sizeof(struct datetime));
+			updateDatetime(envelope);
 		} 
 		if(ev == demo_licht_released_event) {
-			updateDatetime(nighttime);
+			memcpy(&(envelope->value.data), &(nighttime), sizeof(struct datetime));
+			updateDatetime(envelope);
 		}
 	}
 	PROCESS_END();
--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/treppenlichtdemo/treppenlichtdemo.h
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/treppenlichtdemo/treppenlichtdemo.h
@@ -9,3 +9,5 @@ PROCESS_NAME(treppenlicht_demo_process);
 extern process_event_t demo_licht_pressed_event;
 extern process_event_t demo_licht_released_event;
 extern process_event_t demo_licht_clicked_event;
+
+#endif
