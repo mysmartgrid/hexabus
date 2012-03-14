24ee80714435096425fdadad505b5d28bdff4eed
Mathias Dalheimer <md@gonium.net>
2012-03-14 18:05:17 +0100

    added generic error handling header.

--- hostsoftware/hba/libhba/common.cpp
+++ hostsoftware/hba/libhba/common.cpp
@@ -1,20 +1,20 @@
 /**
- * This file is part of libhexabus.
+ * This file is part of hexabus assembler.
  *
  * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>, 2011
  *
- * libhexabus is free software: you can redistribute it and/or modify
+ * hexabus assembler is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * libhexabus is distributed in the hope that it will be useful,
+ * hexabus assembler is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with libhexabus. If not, see <http://www.gnu.org/licenses/>.
+ * along with hexabus assembler. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #include "common.hpp"
---
+++ hostsoftware/hba/libhba/error.hpp
@@ -0,0 +1,60 @@
+/**
+ * This file is part of libhexabus.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>, 2010
+ *
+ * libhexabus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libhexabus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libhexabus. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef LIBHBA_ERROR_HPP
+#define LIBHBA_ERROR_HPP 1
+
+#include <string>
+#include <exception>
+
+namespace hexabus {
+  class GenericException : public std::exception {
+	public:
+	  typedef std::tr1::shared_ptr<GenericException> Ptr;
+	  GenericException (const std::string reason) : _reason(reason) {};
+	  virtual ~GenericException() throw() {};
+	  virtual const char* what() const throw() { return reason().c_str(); }
+	  virtual const std::string& reason() const { return _reason; }
+
+	private:
+	  std::string _reason;
+  };
+
+  class CommunicationException : public GenericException{
+	public:
+	  typedef std::tr1::shared_ptr<CommunicationException> Ptr;
+	  CommunicationException (const std::string reason) :
+		hexabus::GenericException(reason) {};
+	  virtual ~CommunicationException() throw() {};
+
+  };
+  
+  class VertexNotFoundException : public GenericException{
+	public:
+	  typedef std::tr1::shared_ptr<VertexNotFoundException> Ptr;
+	  VertexNotFoundException (const std::string reason) :
+		hexabus::GenericException(reason) {};
+	  virtual ~VertexNotFoundException() throw() {};
+
+  };
+
+}
+
+#endif /* LIBHBA_ERROR_HPP */
