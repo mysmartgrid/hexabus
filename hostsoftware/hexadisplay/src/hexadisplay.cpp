5f9f52b381ea473803ea38a6372fba8ec3e602bf
Mathias Dalheimer <md@gonium.net>
2012-05-21 14:14:07 +0200

    added boilerplate klio sensor readout, commandline parser

--- hostsoftware/hexadisplay/src/hexadisplay.cpp
+++ hostsoftware/hexadisplay/src/hexadisplay.cpp
@@ -1,16 +1,120 @@
+/**
+ * This file is part of hexadisplay.
+ *
+ * (c) Fraunhofer ITWM - Mathias Dalheimer <dalheimer@itwm.fhg.de>, 2012
+ *
+ * hexadisplay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * hexadisplay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hexadisplay. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <QApplication>
 #include <QLabel>
 #include <common.hpp>
 #include <iostream>
 
+#include <libklio/store.hpp>
+#include <libklio/store-factory.hpp>
+#include <libklio/sensor.hpp>
+#include <libklio/sensorfactory.hpp>
+#include <libklio/sensor.hpp>
+#include <libklio/time.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/filesystem.hpp>
+namespace bfs = boost::filesystem; 
+namespace po = boost::program_options;
+
+
+
 int main (int argc, char* argv[]) {
-  hexadisplay::VersionInfo::Ptr version(new hexadisplay::VersionInfo());
-  std::cout << "HexaDisplay V" << version->getVersion() << std::endl;
+  try {
+    std::ostringstream oss;
+    oss << "Usage: " << argv[0] << " ACTION [additional options]";
+    po::options_description desc(oss.str());
+    desc.add_options()
+      ("help,h", "produce help message")
+      ("version,v", "print libklio version and exit")
+      ("storefile,s", po::value<std::string>(), "the data store to use")
+      ;
+    po::positional_options_description p;
+    p.add("storefile", 1);
 
-  QApplication app(argc, argv);
-  QLabel testLabel("foobar");
-  testLabel.show();
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+        options(desc).positional(p).run(), vm);
+    po::notify(vm);
 
-  return app.exec();
+    // Begin processing of commandline parameters.
+    std::string storefile;
+
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 1;
+    }
+
+    if (vm.count("version")) {
+      hexadisplay::VersionInfo::Ptr version(new hexadisplay::VersionInfo());
+      std::cout << "HexaDisplay version " << version->getVersion() << std::endl;
+      klio::VersionInfo::Ptr vi(new klio::VersionInfo());
+      std::cout << "klio library version " << vi->getVersion() << std::endl;
+      return 0;
+    }
+
+    if (! vm.count("storefile")) {
+      std::cerr << "You must specify a store to work on." << std::endl;
+      return 1;
+    } else {
+      storefile=vm["storefile"].as<std::string>();
+    }
+
+    bfs::path db(storefile);
+    klio::StoreFactory::Ptr factory(new klio::StoreFactory()); 
+    klio::SensorFactory::Ptr sensor_factory(new klio::SensorFactory());
+
+    try {
+      klio::Store::Ptr store(factory->openStore(klio::SQLITE3, db));
+      std::cout << "opened store: " << store->str() << std::endl;
+      std::vector<klio::Sensor::uuid_t> uuids = store->getSensorUUIDs();
+      std::cout << "Found " << uuids.size() 
+        << " sensor(s) in the database." << std::endl;
+      std::vector<klio::Sensor::uuid_t>::iterator it;
+      for(  it = uuids.begin(); it < uuids.end(); it++) {
+        klio::Sensor::Ptr loadedSensor(store->getSensor(*it));
+        std::cout << " * " << loadedSensor->str() << std::endl;
+        klio::reading_t reading = store->get_last_reading(loadedSensor);
+        klio::timestamp_t ts1=reading.first;
+        double val1=reading.second;
+        std::cout << ts1 << "\t" << val1 << std::endl;
+      }
+
+    } catch (klio::StoreException const& ex) {
+      std::cout << "Failed to access store: " << ex.what() << std::endl;
+      exit(-1);
+    }
+
+    //  QApplication app(argc, argv);
+    //  QLabel testLabel("foobar");
+    //  testLabel.show();
+    //
+    //  return app.exec();
+  } catch(std::exception& e) {
+    std::cerr << "error: " << e.what() << std::endl;
+    return 1;
+  } catch(...) {
+    std::cerr << "Exception of unknown type!" << std::endl;
+    return 1;
+  }
 }
-
