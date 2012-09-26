1a2be36a0de0335e920baf2262e1ecd35f175048
Bernd Lietzow <github@ebrnd.de>
2012-09-26 16:42:02 +0200

    writing down some thoughts

--- hostsoftware/hbc/libhbc/graph_transformation.cpp
+++ hostsoftware/hbc/libhbc/graph_transformation.cpp
@@ -11,4 +11,11 @@ void GraphTransformation::operator()(graph_t_ptr in_g) {
   // * if a device appears in multiple state machines:    |   (at this point one dev. can have several state machines)
   //   -> parallel compose state machine slices           | * now, each dev. only has one st.m. (store as map?)
   // * simplify slices for Hexabus Assembler export       | * use the same map data structure for this
+  //
+  // different version:
+  // we have a table of devices. we could use this to get a list of device names / ip addresses.
+  // but: a lot of devices which are write only are in this table. we should use it only to find out
+  // whether a device exists, and has the right access levels (write (or something like localwrite)
+  // for EPs which are written, broadcast for EPs which conditions depend upon. BUT we should check
+  // this in the assembler export stage rather than here.
 }
