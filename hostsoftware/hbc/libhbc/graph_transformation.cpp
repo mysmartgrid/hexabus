cb5a5dfb48f65339f76893dcdfd9f588cf1417a5
Bernd Lietzow <github@ebrnd.de>
2012-09-27 11:50:51 +0200

    writing down more thoughts. time to get coding.

--- hostsoftware/hbc/libhbc/graph_transformation.cpp
+++ hostsoftware/hbc/libhbc/graph_transformation.cpp
@@ -18,4 +18,18 @@ void GraphTransformation::operator()(graph_t_ptr in_g) {
   // whether a device exists, and has the right access levels (write (or something like localwrite)
   // for EPs which are written, broadcast for EPs which conditions depend upon. BUT we should check
   // this in the assembler export stage rather than here.
+
+  // Iterate over all vertices, get state machine IDs
+  // For each state machine ID:
+  //   Construct set of states for this ID (vector or something  -- ooh! ooh! Map<st.m.id,vector<state>>)
+  //   Do magic with this set.
+  //     Magic: Look for all device names in the machine.
+  //     FOR each device name, #slice# it -- slices are magic items we need.
+
+  // Sliced machines which fall out of the magic cauldron:
+  //   They have to be stored in some new set.
+  //   I'd like to have a map<device name,machine>, but at this point, we can have multiple machines with the same device name
+  //      -> map<pair<device,stm.id>,machine> perhaps???
+  //   Now, for the second bit of magic: IF there is a device name with multiple state machines attached to it
+  //      Parallel compose the state machines.
 }
