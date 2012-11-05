c3210cac2e00b46b66e042fff98b021bbaa9f3c4
Bernd Lietzow <github@ebrnd.de>
2012-11-05 15:21:39 +0100

    class for graph checks

---
+++ hostsoftware/hbc/libhbc/graph_checks.cpp
@@ -0,0 +1,16 @@
+#include "graph_checks.hpp"
+
+#include <libhbc/error.hpp>
+
+using namespace hexabus;
+
+void GraphChecks::operator()(graph_t_ptr _g) {
+  // TODO implement at the dotted line
+  //
+  //
+  // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+  //
+  //
+  //
+}
+
---
+++ hostsoftware/hbc/libhbc/graph_checks.hpp
@@ -0,0 +1,26 @@
+#ifndef LIBHBC_GRAPH_CHECKS
+#define LIBHBC_GRAPH_CHECKS
+
+#include <libhbc/common.hpp>
+#include <libhbc/graph.hpp>
+#include <libhbc/tables.hpp>
+#include <boost/graph/breadth_first_search.hpp>
+#include <boost/graph/visitors.hpp>
+#include <iostream>
+
+namespace hexabus {
+
+  class GraphChecks {
+    public:
+      typedef std::tr1::shared_ptr<GraphChecks> Ptr;
+      GraphChecks() {};
+      virtual ~GraphChecks() {};
+
+      void operator()(graph_t_ptr _g);
+
+    private:
+
+  };
+};
+
+#endif // LIBHBC_GRAPH_CHECKS
