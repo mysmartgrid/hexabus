8725673a46ddc3bbb1defee4159c3053a0a9b60a
myeisha <github@quasiparticle.net>
2012-11-09 11:23:59 +0100

    change multicast group used to ff05::114 (rsvd for private experiments)

--- shared/hexabus_definitions.h
+++ shared/hexabus_definitions.h
@@ -5,8 +5,8 @@
 // Definitions for fields
 
 #define HXB_PORT              61616
-#define HXB_GROUP             "ff05::1"                           // keep these in sync. the second define is used in
-#define HXB_GROUP_RAW         0xff05, 0, 0, 0, 0, 0, 0, 0x0001    // hexaplug code.
+#define HXB_GROUP             "ff05::114"                         // keep these in sync. the second define is used in
+#define HXB_GROUP_RAW         0xff05, 0, 0, 0, 0, 0, 0, 0x0114    // hexaplug code.
 #define HXB_HEADER            "HX0C"
 
 // Boolean values
