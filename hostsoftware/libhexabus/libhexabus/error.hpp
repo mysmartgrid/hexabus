9cf7e0dd9eaa5e9f1674eb230f423d8a9f15e513
myeisha <github@quasiparticle.net>
2012-11-23 15:14:32 +0100

    add forgotten header file
    !#@*

---
+++ hostsoftware/libhexabus/libhexabus/error.hpp
@@ -0,0 +1,31 @@
+#ifndef LIBHEXABUS_ERROR_HPP
+#define LIBHEXABUS_ERROR_HPP 1
+
+#include <string>
+#include <exception>
+
+namespace hexabus {
+  class GenericException : public std::exception {
+    public:
+      typedef std::tr1::shared_ptr<GenericException> Ptr;
+      GenericException (const std::string reason) : _reason(reason) {};
+      virtual ~GenericException() throw() {};
+      virtual const char* what() const throw() { return reason().c_str(); }
+      virtual const std::string& reason() const { return _reason; }
+
+    private:
+      std::string _reason;
+  };
+
+  class NetworkException : public GenericException {
+    public:
+      typedef std::tr1::shared_ptr<NetworkException> Ptr;
+      NetworkException (const std::string reason) :
+        hexabus::GenericException(reason) {};
+      virtual ~NetworkException() throw() {};
+
+  };
+
+}
+
+#endif /* LIBHEXABUS_ERROR_HPP */
