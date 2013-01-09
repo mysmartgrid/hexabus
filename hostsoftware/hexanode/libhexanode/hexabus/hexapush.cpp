72afe40e196cbd6add49683024887f52e9acf4f2
myeisha <github@quasiparticle.net>
2013-01-09 15:26:43 +0100

    port hexanode to new libhexabus

--- hostsoftware/hexanode/libhexanode/hexabus/hexapush.cpp
+++ hostsoftware/hexanode/libhexanode/hexabus/hexapush.cpp
@@ -2,27 +2,11 @@
 
 using namespace hexanode;
 
-HexaPush::HexaPush ()
-  : _network(new hexabus::NetworkAccess(hexabus::NetworkAccess::Reliable))
-  , _packetm(new hexabus::Packet())
+HexaPush::HexaPush (hexabus::Socket& socket)
+  : _socket(socket)
 {
-}
-
-HexaPush::HexaPush(const std::string& interface) 
-  : _network(new hexabus::NetworkAccess(interface, hexabus::NetworkAccess::Reliable))
-  , _packetm(new hexabus::Packet())
-{
-  std::cout << "Using interface " << interface << std::endl;
 }
 
 void HexaPush::on_event(uint8_t pressed_key) {
-  uint32_t eid = 25;
-  unsigned int dtype = HXB_DTYPE_UINT8;
-  struct hxb_packet_int8 packet8;
-  packet8 = _packetm->write8(eid, dtype, pressed_key, true);
-  _network->sendPacket(
-      HXB_GROUP, 
-      HXB_PORT, 
-      (char*)&packet8, 
-      sizeof(packet8));
+  _socket.send(hexabus::InfoPacket<uint8_t>(25, pressed_key));
 }
