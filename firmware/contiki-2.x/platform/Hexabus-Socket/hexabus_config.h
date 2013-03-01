3598afa3368eb5734da906e5488424133a9d4c78
myeisha <github@quasiparticle.net>
2013-03-01 16:01:23 +0100

    disable state machine debug

--- firmware/contiki-2.x/platform/Hexabus-Socket/hexabus_config.h
+++ firmware/contiki-2.x/platform/Hexabus-Socket/hexabus_config.h
@@ -60,7 +60,7 @@ There should be an ENABLE option that enables compilation and execution of the a
 
 // state_machine
 #define STATE_MACHINE_ENABLE 1
-#define STATE_MACHINE_DEBUG 1
+#define STATE_MACHINE_DEBUG 0
 
 // state machine uploading via Hexabus packets
 #define SM_UPLOAD_ENABLE 1
