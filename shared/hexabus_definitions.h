ae9bbf690248044bc0f5e0fb8eae6dfaa5d8b817
Frederik Walk <walk@itwm.fraunhofer.de>
2013-11-20 16:04:15 +0100

    Updated shared definitions for HX0D protcol version

--- shared/hexabus_definitions.h
+++ shared/hexabus_definitions.h
@@ -7,7 +7,7 @@
 #define HXB_PORT              61616
 #define HXB_GROUP             "ff05::205"                         // keep these in sync. the second define is used in
 #define HXB_GROUP_RAW         0xff05, 0, 0, 0, 0, 0, 0, 0x0205    // hexaplug code.
-#define HXB_HEADER            "HX0C"
+#define HXB_HEADER            "HX0D"
 
 #define HXB_STRING_PACKET_MAX_BUFFER_LENGTH 127
 #define HXB_66BYTES_PACKET_MAX_BUFFER_LENGTH 65
