b85a866e810be4d37e26b2c2046d5845adcdb018
Frederik Walk <walk@itwm.fraunhofer.de>
2014-01-15 15:01:50 +0100

    nothing important...

--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/udp_handler/reliability.c
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/udp_handler/reliability.c
@@ -16,8 +16,15 @@ enum hxb_send_state_t {
 	FAILED		= 3,
 };
 
+enum hxb_recv_state_t {
+	INIT 		= 0,
+	READY 		= 1,
+	FAILED 		= 2,
+};
+
 static enum hxb_send_state_t send_state = INIT;
-static bool fail;
+static enum hxb_recv_state_t recv_state = INIT;
+static uint8_t fail;
 static bool ack;
 static uint8_t retrans;
 static uint16_t want_ack_for;
@@ -51,8 +58,13 @@ void process_ack(uint16_t sequence_number) {
 }
 
 enum hxb_error_code enqueue_packet(struct hxb_queue_packet* packet) {
+
+	struct hxb_queue_packet* queue_entry = malloc(sizeof(struct hxb_queue_packet));
+
 	if(packet != NULL) {
-		if(packetqueue_enqueue_packetbuf(&send_queue, 0, (void*) packet)) {
+		memcpy(queue_entry, packet, sizeof(struct hxb_queue_packet));
+
+		if(packetqueue_enqueue_packetbuf(&send_queue, 0, (void*) queue_entry)) {
 			return HXB_ERR_OUT_OF_MEMORY;
 		}
 	} else {
@@ -67,7 +79,7 @@ static void run_send_state_machine() {
 
 	switch(send_state) {
 		case INIT:
-			fail = false;
+			fail = 0;
 			send_state = READY;
 			break;
 		case READY:
@@ -86,20 +98,37 @@ static void run_send_state_machine() {
 		case WAIT_ACK:
 			if(ack) {
 				want_ack_for = 0;
+				free(packetqueue_first(&send_queue));
 				packetqueue_dequeue(&send_queue);
-				//TODO free buffer
 				send_state = READY;
 			} else if(etimer_expired(&timeout_timer)) {
 				if(retrans++ < RETRANS_LIMIT) {
 					send_packet((struct hxb_queue_packet *)packetqueue_first(&send_queue));
 					etimer_set(&timeout_timer, RETRANS_TIMEOUT);
 				} else {
+					fail = 1;
 					send_state = FAILED;
 				}
 			}
 			break;
 		case FAILED:
-			//huh?
+			if(fail == 2) {
+				//TODO recover
+			}
+			break;
+	}
+}
+
+static void run_recv_state_machine() {
+	switch(recv_state) {
+		case INIT:
+
+			break;
+		case READY:
+
+			break;
+		case FAILED:
+
 			break;
 	}
 }
@@ -114,6 +143,7 @@ PROCESS_THREAD(reliability_send_process, ev, data)
 
 	while (1) {
 		run_send_state_machine();
+		run_recv_state_machine();
 		//etimer_set(&button_timer, BUTTON_DEBOUNCE_TICKS);
 		PROCESS_PAUSE();
 	}
