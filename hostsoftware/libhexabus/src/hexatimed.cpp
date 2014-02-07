fd6350acc6f429153d419d1dd619e0c190c46cd7
Frederik Walk <f_walk09@cs.uni-kl.de>
2014-02-07 16:41:30 +0100

    Implemented hexatimed

--- hostsoftware/libhexabus/src/hexatimed.cpp
+++ hostsoftware/libhexabus/src/hexatimed.cpp
@@ -12,19 +12,32 @@
 
 namespace po = boost::program_options;
 namespace ba = boost::asio;
+namespace pt = boost::posix_time;
 
 bool verbose = false;
 
-class DatetimeSupply {
-	public:
-		DatetimeSupply(ba::io_serivce& io) {
+enum ErrorCode {
+	ERR_NONE = 0,
 
-		}
+	ERR_UNKNOWN_PARAMETER = 1,
+	ERR_PARAMETER_MISSING = 2,
+	ERR_PARAMETER_FORMAT = 3,
+	ERR_PARAMETER_VALUE_INVALID = 4,
+	ERR_NETWORK = 5,
 
-	private:
-
-
+	ERR_OTHER = 127
 };
+
+void sendTime(const boost::system::error_code& e, boost::asio::deadline_timer* t) {
+	pt::ptime currentTime(pt::second_clock::local_time());
+	send_packet(network, hxb_broadcast_address, hexabus::InfoPacket(0, currentTime));
+	t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+	t->async_wait(boost::bind(sendTime, boost::asio::placeholders::error, t));
+
+	if(verbose) {
+		std::cout << "Updated time to: " << pt::to_simple_string(currentTime) << std::endl;
+	}
+}
 
 int main(int argc, char** argv)
 {
@@ -36,12 +49,10 @@ int main(int argc, char** argv)
 		("help,h", "produce help message")
 		("version", "print version and exit")
 		("interface,I", po::value<std::vector<std::string> >(), "outgoing interface")
-		("nofork,n", "dont't fork")
 		("verbose,V", "print more status information")
 		;
 
 	po::variables_map vm;
-	bool fork = true;
 
 	// begin processing of command line parameters
 	try {
@@ -49,73 +60,52 @@ int main(int argc, char** argv)
 		po::notify(vm);
 	} catch (std::exception& e) {
 		std::cerr << "Cannot process command line: " << e.what() << std::endl;
-		exit(-1);
+		return ERR_UNKNOWN_PARAMETER;
 	}
 
 	if(vm.count("help"))
 	{
 		std::cout << desc << std::endl;
-		return 1;
+		return ERR_NONE;
 	}
 
 	if(vm.count("version"))
 	{
 		std::cout << "hexatimed -- hexabus date time broadcast daemon" << std::endl;
 		std::cout << "libhexabus version " << hexabus::version() << std::endl;
-		return 0;
+		return ERR_NONE;
+	}
+
+	if(!vm.count("interface"))
+	{
+		std::cerr << "You must specify an interface to send mulitcasts from."
+		return ERR_PARAMETER_MISSING;
 	}
 
 	if(vm.count("verbose"))
 		verbose = true;
 
-	if(vm.count("nofork"))
-		fork = false;
-
+	//Setup interface
+	ba::io_serivce io;
+	boost::asio::ip::address_v6 hxb_broadcast_address = boost::asio::ip::address_v6::from_string(HXB_GROUP);
 	try {
-		ba::io_serivce io;
-		DatetimeSupply supply(io);
-
-		ba::signal_set signals(io, SIGINT, SIGTERM);
-    	signals.async_wait(boost::bind(&ba::io_service::stop, &io));
-
-    	io.notify_fork(boost::asio::io_service::fork_prepare);
-
-    	if (pid_t pid = fork()) {
-    		if (pid > 0) {
-    			exit(0);
-    		} else {
-				syslog(LOG_ERR | LOG_USER, "Forking failed: %m");
-				return 1;
-    		}
-    	}
-
-		setsid();
-		chdir("/");
-
-		if (pid_t pid = fork()) {
-			if (pid > 0) {
-				exit(0);
-			} else {
-				syslog(LOG_ERR | LOG_USER, "Forking failed: %m");
-				return 1;
-			}
-		}
-
-		close(0);
-		close(1);
-		close(2);
-
-		io.notify_fork(boost::asio::io_service::fork_child);
-
-		syslog(LOG_INFO | LOG_USER, "hexatimed started");
-		io.run();
-		syslog(LOG_INFO | LOG_USER, "hexatimed stopped");	
-	} catch (std::exception& e) {
-		std::cerr << "Failed to start daemon process: " << e.what() << std::endl;
+		hexabus::Socket* network = new hexabus::Socket(io, vm["interface"].as<std::string>());
+	} catch(const hexabus::NetworkException& e) {
+		std::cerr << "Colud not open socket: " << e.what().message() << std::endl;
+		return ERR_NETWORK;
 	}
+
+	boost::asio::deadline_timer periodicTimer(io, boost::posix_time::seconds(1));
+	periodicTimer.async_wait(boost::bind(sendTime, boost::asio::placeholders::error, &periodicTimer));
+
+	boost::asio::signal_set signals(io_service, SIGINT, SIGTERM);
+	signals.async_wait(boost::bind(&boost::asio::io_service::stop, &io));
+
+	io.run();
+
 
 	if(verbose)
 		std::cout << "Closing..." << std::endl;
 
-	return 0;
+	return ERR_NONE;
 }
