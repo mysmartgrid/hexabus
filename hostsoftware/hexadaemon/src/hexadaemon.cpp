8109b624a25b420b4ca634cca3896317f264da64
paalsteek <github@paalsteek.de>
2014-07-08 15:24:27 +0200

    [hexadaemon] updated copyright header and cleaned up includes

--- hostsoftware/hexadaemon/src/hexadaemon.cpp
+++ hostsoftware/hexadaemon/src/hexadaemon.cpp
@@ -3,21 +3,18 @@
 // ~~~~~~~~~~
 //
 // Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2013-2014 Stephan Platz (platz at itwm dot fraunhofer dot de)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <boost/asio/io_service.hpp>
-//#include <boost/asio/ip/udp.hpp>
 #include <boost/asio/signal_set.hpp>
-//#include <boost/array.hpp>
 #include <boost/bind.hpp>
 #include <boost/program_options.hpp>
-//#include <ctime>
 #include <iostream>
 #include <syslog.h>
-//#include <unistd.h>
 
 namespace po = boost::program_options;
 
