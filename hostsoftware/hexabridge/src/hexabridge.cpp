25f48e56b0941d3cb229325ab9b51961b74e9140
paalsteek <github@paalsteek.de>
2014-07-10 14:43:03 +0200

    initial commit for hexabridge

---
+++ hostsoftware/hexabridge/src/hexabridge.cpp
@@ -0,0 +1,106 @@
+/**
+ * This file is part of hexabridge.
+ *
+ * (c) Fraunhofer ITWM - Stephan Platz <platz@itwm.fhg.de>, 2014
+ *
+ * hexabridge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hexabridge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hexabridge. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "hexabridge.hpp"
+
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+#include <boost/ref.hpp>
+
+#include <libhexabus/device.hpp>
+#include <libhexabus/endpoint_registry.hpp>
+
+#include <libklio/store-factory.hpp>
+
+using namespace hexabridge;
+
+namespace bf = boost::filesystem;
+
+std::string _device_name = "HexaBridge";
+uint32_t eid = 2;
+
+Pusher::Pusher(boost::asio::io_service& io, const std::vector<std::string>& interfaces, const std::vector<std::string>& addresses,
+		const std::string& url, const std::string& id, const std::string& token, int interval, bool debug)
+	: _device(io, interfaces, addresses, interval)
+	, _debug(debug)
+{
+	klio::StoreFactory store_factory;
+
+	//TODO: url handling
+	_store = store_factory.create_msg_store();
+
+	klio::SensorFactory sensor_factory;
+	_sensor = sensor_factory.create_msg_sensor(
+		"uid123",
+		id,
+		"test",
+		"description",
+		"watt",
+		"Europe/Berlin",
+		token);
+	_store->add_sensor(_sensor);
+
+	_init();
+}
+
+void Pusher::_init() {
+	_device.onReadName(boost::bind(&Pusher::loadDeviceName, this));
+	_device.onWriteName(boost::bind(&Pusher::saveDeviceName, this, _1));
+
+	hexabus::EndpointRegistry ep_registry;
+	hexabus::EndpointRegistry::const_iterator ep_it;
+
+	ep_it = ep_registry.find(eid);
+	hexabus::TypedEndpointFunctions<uint32_t>::Ptr powerEP = ep_it != ep_registry.end()
+		? hexabus::TypedEndpointFunctions<uint32_t>::fromEndpointDescriptor(ep_it->second)
+		: hexabus::TypedEndpointFunctions<uint32_t>::Ptr(new hexabus::TypedEndpointFunctions<uint32_t>(eid, "HexabusPlug+ Power meter (W)"));
+	powerEP->onRead(boost::bind(&Pusher::get_last_reading, this));
+	_device.addEndpoint(powerEP);
+}
+
+uint32_t Pusher::get_last_reading() {
+	try {
+		klio::reading_t reading = _store->get_last_reading(_sensor);
+		std::cout << reading.first << ": " << reading.second << std::endl;
+		return reading.second;
+	} catch (klio::GenericException e) {
+		std::cerr << "Error during fetch of sensor data: " << e.what() << std::endl;
+	}
+	return 0;
+}
+
+std::string Pusher::loadDeviceName()
+{
+	_debug && std::cout << "loading device name" << std::endl;
+	std::string name;
+	name = _device_name;
+
+	return name;
+}
+
+void Pusher::saveDeviceName(const std::string& name)
+{
+	_debug && std::cout << "saving device name \"" << name << "\"" << std::endl;
+	if ( name.size() > 30 )
+	{
+		std::cerr << "cannot save a device name of length > 30" << std::endl;
+		return;
+	}
+
+	_device_name = name;
+}
---
+++ hostsoftware/hexabridge/src/hexabridge.hpp
@@ -0,0 +1,52 @@
+/**
+ * This file is part of hexabridge.
+ *
+ * (c) Fraunhofer ITWM - Stephan Platz <platz@itwm.fhg.de>, 2014
+ *
+ * hexabridge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hexabridge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hexabridge. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _HEXABRIDGE_HPP
+#define _HEXABRIDGE_HPP
+
+#include <boost/asio/io_service.hpp>
+
+#include <libhexabus/device.hpp>
+
+#include <libklio/msg/msg-sensor.hpp>
+#include <libklio/msg/msg-store.hpp>
+
+namespace hexabridge {
+	class Pusher {
+		public:
+			typedef boost::shared_ptr<Pusher> Ptr;
+			Pusher(boost::asio::io_service& io, const std::vector<std::string>& interfaces, const std::vector<std::string>& addresses, const std::string& url, const std::string& id, const std::string& token, int interval = 60, bool debug = false);
+			virtual ~Pusher() {};
+
+			std::string loadDeviceName();
+			void saveDeviceName(const std::string& name);
+
+			uint32_t get_last_reading();
+
+		private:
+			void _init();
+
+		private:
+			hexabus::Device _device;
+			klio::MSGStore::Ptr _store;
+			klio::MSGSensor::Ptr _sensor;
+			bool _debug;
+	};
+}
+
+#endif // _HEXABRIDGE_HPP
