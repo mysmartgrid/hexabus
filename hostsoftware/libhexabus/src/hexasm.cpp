ec425e16a5e278b8c1011e8aaca80fc175e4557a
myeisha <github@quasiparticle.net>
2014-07-15 13:53:46 +0200

    update hexasm to accept hbt-as output

--- hostsoftware/libhexabus/src/hexasm.cpp
+++ hostsoftware/libhexabus/src/hexasm.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <utility>
 #include <vector>
 
@@ -5,85 +6,6 @@
 
 #include "libhexabus/sm/machine.hpp"
 #include "libhexabus/socket.hpp"
-
-struct Label {
-};
-
-class Assembler {
-	private:
-		struct Fixup {
-			uint16_t addr;
-			uint16_t offset_from;
-			bool offset_set;
-			const Label* label;
-			bool is_absolute;
-		};
-
-		std::vector<uint8_t> _program;
-		std::vector<Fixup> _fixups;
-
-	public:
-		Assembler& operator()(uint8_t val)
-		{
-			_program.push_back(val);
-			return *this;
-		}
-
-		Assembler& operator()(enum hexabus::sm::hxb_sm_opcode op)
-		{
-			for (size_t i = 0; i < _fixups.size(); i++) {
-				if (_fixups[i].offset_set)
-					continue;
-
-				_fixups[i].offset_from = _program.size();
-				_fixups[i].offset_set = true;
-			}
-
-			return (*this)((uint8_t) op);
-		}
-
-		Assembler& operator()(uint16_t val)
-		{ return (*this)((uint8_t) (val >> 8))((uint8_t) (val & 0xFF)); }
-
-		Assembler& operator()(uint32_t val)
-		{ return (*this)((uint16_t) (val >> 16))((uint16_t) (val & 0xFFFF)); }
-
-		Assembler& operator()(float val)
-		{
-			uint32_t u;
-			memcpy(&u, &val, 4);
-			return (*this)(u);
-		}
-
-		Assembler& operator()(const Label& label, bool is_absolute = false)
-		{
-			Fixup f = { _program.size(), 0, false, &label, is_absolute };
-			_fixups.push_back(f);
-			return (*this)((uint16_t) 0);
-		}
-
-		Assembler& mark(const Label& label)
-		{
-			for (size_t i = 0; i < _fixups.size(); i++) {
-				if (_fixups[i].label != &label)
-					continue;
-
-				uint16_t diff = _fixups[i].is_absolute
-					? _program.size()
-					: _program.size() - _fixups[i].offset_from;
-
-				_program[_fixups[i].addr + 0] = diff >> 8;
-				_program[_fixups[i].addr + 1] = diff & 0xFF;
-			}
-
-			return *this;
-		}
-
-		std::vector<uint8_t> finish()
-		{
-			return _program;
-		}
-};
 
 static void on_packet(hexabus::sm::Machine& machine,
 		const hexabus::Packet& packet,
@@ -153,71 +75,45 @@ static uint8_t on_write(uint32_t eid, hexabus::sm::Machine::write_value_t value)
 	return 0;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 	using namespace hexabus::sm;
 
-	typedef uint8_t u8;
-	typedef uint16_t u16;
-	typedef uint32_t u32;
+	if (argc < 3) {
+		std::cout << "Usage: hexasm <iface> <sm-file>\n";
+		return 1;
+	}
 
-	Assembler as;
-	Label on_packet, on_periodic, my_packet, in_state_on, in_state_off, end_program;
+	std::string iface(argv[1]);
+	std::ifstream file(argv[2], std::ios::binary | std::ios::ate);
 
-	as
-		((uint8_t) 0)
-		(on_packet, true)
-		(on_periodic, true)
-	.mark(on_packet)
-		(HSO_LD_SOURCE_IP)
-		(HSO_CMP_BLOCK)((u8) 0x0f)((u32) 0xfdac814e)((u32) 0x24567334)((u32) 0x69ef7d2f)((u32) 0xcd3df75f)
-		(HSO_LD_SOURCE_EID)
-		(HSO_LD_U8)((u8) 20)
-		(HSO_CMP_EQ)
-		(HSO_OP_AND)
-		(HSO_JNZ)(my_packet)
-		(HSO_JUMP)(end_program)
-	.mark(my_packet)
-		(HSO_LD_CURSTATE)
-		(HSO_OP_SWITCH_8)((u8) 2)
-			((u8) 0)(in_state_off)
-			((u8) 1)(in_state_on)
-		(HSO_RET_STAY)
-	.mark(in_state_off)
-		(HSO_LD_SOURCE_VAL)
-		(HSO_LD_U8)((u8) 11)
-		(HSO_CMP_EQ)
-		(HSO_JZ)(end_program)
-			(HSO_LD_U8)((u8) 2)
-			(HSO_LD_TRUE)
-			(HSO_WRITE)
-			(HSO_POP)
-		(HSO_LD_TRUE)
-		(HSO_RET_CHANGE)
-	.mark(in_state_on)
-		(HSO_LD_SOURCE_VAL)
-		(HSO_LD_U8)((u8) 12)
-		(HSO_CMP_EQ)
-		(HSO_JZ)(end_program)
-			(HSO_LD_U8)((u8) 2)
-			(HSO_LD_FALSE)
-			(HSO_WRITE)
-			(HSO_POP)
-		(HSO_LD_FALSE)
-		(HSO_RET_CHANGE)
-	.mark(end_program)
-	.mark(on_periodic)
-		(HSO_RET_STAY)
-	;
+	if (file.fail()) {
+		std::cerr << "can't open file " << argv[2] << "\n";
+		return 1;
+	}
+	if (file.tellg() < 16) {
+		std::cerr << "file too short\n";
+		return 1;
+	}
 
-	Machine machine(as.finish());
+	std::vector<uint8_t> machineCode;
+
+	machineCode.resize(std::streamoff(file.tellg()) - 16);
+	file.seekg(16, std::ios::beg);
+	file.read((char*) &machineCode[0], machineCode.size());
+	if (file.fail()) {
+		std::cerr << "can't read file " << argv[2] << "\n";
+		return 1;
+	}
+
+	Machine machine(machineCode);
 	machine.onWrite(on_write);
 
 	boost::asio::io_service io;
 	hexabus::Listener listener(io);
 	boost::asio::deadline_timer timer(io);
 
-	listener.listen("eth1");
+	listener.listen(iface);
 	listener.onPacketReceived(boost::bind(::on_packet, boost::ref(machine), _1, _2));
 
 	::on_periodic(machine, timer);
