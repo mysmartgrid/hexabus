5dda14d1630a90d597d0e74363f4c1cb5c586e1a
myeisha <github@quasiparticle.net>
2014-08-27 12:58:32 +0200

    minor header formatting

--- firmware/platform/Hexabus-STM/hexabus_platform_config.h
+++ firmware/platform/Hexabus-STM/hexabus_platform_config.h
@@ -2,19 +2,21 @@
 #define HEXABUS__PLATFORM__CONFIG_H_F09C378DE675F16F
 
 typedef uint16_t button_pin_t;
-# include <stm32l1xx.h>
-# include <string.h>
-# include <stdio.h>
-# define RODATA
-# define ROSTR(x)                  (x)
-# define ROSTR_FMT                 "s"
-# define MAX_BUTTON_BIT            (0x8000)
-# define memcpy_from_rodata(...)   memcpy(__VA_ARGS__)
-# define strncpy_from_rodata(...)  strncpy(__VA_ARGS__)
-# define printf_rofmt(...)         printf(__VA_ARGS__)
 
-#define BUTTON_PIN            GPIOA->IDR
-#define BUTTON_BIT            0
-#define BUTTON_ACTIVE_LEVEL   1
+#include <stm32l1xx.h>
+#include <string.h>
+#include <stdio.h>
+
+#define RODATA
+#define ROSTR(x)                  (x)
+#define ROSTR_FMT                 "s"
+#define MAX_BUTTON_BIT            (0x8000)
+#define memcpy_from_rodata(...)   memcpy(__VA_ARGS__)
+#define strncpy_from_rodata(...)  strncpy(__VA_ARGS__)
+#define printf_rofmt(...)         printf(__VA_ARGS__)
+
+#define BUTTON_PIN          GPIOA->IDR
+#define BUTTON_BIT          0
+#define BUTTON_ACTIVE_LEVEL 1
 
 #endif
