670309d45cb61d4a3b6ac09c821fab4e4e01ff28
myeisha <github@quasiparticle.net>
2014-10-26 16:11:51 +0100

    add machine collector to Lang
    collectMachines walks a TU and determines, for each device, which
    machine of the TU must run on the device for the distributed program to
    be valid. A machine must run on a device whenever that machine writes to
    the device, in all other cases, the machine needn't run there.

--- hostsoftware/hbt/include/Lang/codegen.hpp
+++ hostsoftware/hbt/include/Lang/codegen.hpp
@@ -1,7 +1,9 @@
 #ifndef INCLUDE_LANG_CODEGEN_HPP_127703CDCA9B55E7
 #define INCLUDE_LANG_CODEGEN_HPP_127703CDCA9B55E7
 
+#include <map>
 #include <memory>
+#include <set>
 
 namespace hbt {
 
@@ -11,7 +13,11 @@ class Program;
 
 namespace lang {
 
+class Device;
+class MachineDefinition;
 class TranslationUnit;
+
+std::map<const Device*, std::set<MachineDefinition*>> collectMachines(TranslationUnit& tu);
 
 std::unique_ptr<mc::Program> generateMachineCodeFor(TranslationUnit& tu);
 
