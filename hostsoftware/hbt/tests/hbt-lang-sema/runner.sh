a5efdec3c4e72468021814620d4d72e1efb2123a
myeisha <github@quasiparticle.net>
2015-01-06 16:51:19 +0100

    use hbc name from cmake for tests, not some hardcoded path

--- hostsoftware/hbt/tests/hbt-lang-sema/runner.sh
+++ hostsoftware/hbt/tests/hbt-lang-sema/runner.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 dir=$(dirname $0)
+hbc=$1
 total=0
 failed=0
 
 for file in $dir/*.hbc
 do
 	total=$(($total + 1))
-	if $dir/../../build/src/hbc -fsyntax-only $file
+	if $hbc -fsyntax-only $file
 	then
 		echo "PASS: " $(basename $file)
 	else
