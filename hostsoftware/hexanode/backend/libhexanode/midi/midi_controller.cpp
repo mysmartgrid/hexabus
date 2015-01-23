fdda979cbe634f9f201e002eb7c8c3aa39fa7cf7
myeisha <github@quasiparticle.net>
2015-01-23 15:49:43 +0100

    update midi2hexabus to behave like a hexabus device
    most importantly: make midi2hexabus enumerable by hexinfo. can't change
    the name, though.

--- hostsoftware/hexanode/backend/libhexanode/midi/midi_controller.cpp
+++ hostsoftware/hexanode/backend/libhexanode/midi/midi_controller.cpp
@@ -1,4 +1,6 @@
 #include "midi_controller.hpp"
+
+#include "config.h"
 #include <libhexanode/error.hpp>
 
 using namespace hexanode;
@@ -7,82 +9,76 @@ using namespace hexanode;
 # define RTMIDI_API RtMidi::LINUX_ALSA
 #endif
 
-MidiController::MidiController() 
-  : _t()
-  , _terminate_midievent_loop(false)
-  , _midiin()
-  , _on_midievent()
-{ };
-
-MidiController::~MidiController() {
-  if (_midiin != NULL)
-    delete _midiin;
-}
-
-void MidiController::open() {
-  try {
-    _midiin = new RtMidiIn(RTMIDI_API);
-  } catch (const RtError& error) {
-    throw MidiException(error.getMessage());
-  }
-}
-
-boost::signals2::connection MidiController::do_on_midievent(
-    uint16_t port,
-    const on_midievent_slot_t& slot)
+MidiController::MidiController()
+	: _terminate_midievent_loop(false)
 {
-    _midiin->openPort( port );
-    // Don't ignore sysex, timing, or active sensing messages.
-    _midiin->ignoreTypes( false, false, false );
-
-    //_midiin->setCallback( &internal_callback );
-  
-  return _on_midievent.connect(slot);
 }
 
-void MidiController::run() {
-  _t = boost::thread(
-        boost::bind(&MidiController::midievent_loop, this)
-      );
+MidiController::~MidiController()
+{
+	if (_t.joinable())
+		shutdown();
 }
 
-void MidiController::shutdown() {
-  _terminate_midievent_loop=true;
-  _t.join();
+void MidiController::open()
+{
+	try {
+		_midiin.reset(new RtMidiIn(RTMIDI_API));
+	} catch (const RtError& error) {
+		throw MidiException(error.getMessage());
+	}
 }
 
-void MidiController::midievent_loop() {
-  std::vector<unsigned char> message;
-  int nBytes;
-  //double stamp;
-  while (! _terminate_midievent_loop) {
-    //stamp = _midiin->getMessage( &message );
-    _midiin->getMessage( &message );
-    nBytes = message.size();
-    if (nBytes > 0) {
-      _on_midievent(&message);
-      //std::cout << "stamp = " << stamp << std::endl;
-    }
+boost::signals2::connection MidiController::do_on_midievent(uint16_t port, const on_midievent_slot_t& slot)
+{
+	_midiin->openPort(port);
+	// Don't ignore sysex, timing, or active sensing messages.
+	_midiin->ignoreTypes(false, false, false);
 
-    // Sleep for 10 milliseconds ... platform-dependent.
-    boost::this_thread::sleep(boost::posix_time::milliseconds(2));
-  }
+	return _on_midievent.connect(slot);
 }
 
-uint16_t MidiController::num_ports() {
-  uint16_t retval=0;
-  try {
-    retval = _midiin->getPortCount();
-  } catch (const RtError& error) {
-    throw MidiException(error.getMessage());
-  }
-  return retval;
+void MidiController::run()
+{
+	_t = std::thread([&] () {
+		midievent_loop();
+	});
 }
 
-void MidiController::print_ports() {
-  std::string portName;
-  for ( unsigned int i=0; i<num_ports(); i++ ) {
-    portName = _midiin->getPortName(i);
-    std::cout << "  Input Port #" << i+1 << ": " << portName << std::endl;
-  }
+void MidiController::shutdown()
+{
+	_terminate_midievent_loop = true;
+	_t.join();
+}
+
+void MidiController::midievent_loop()
+{
+	std::vector<unsigned char> message;
+	int nBytes;
+
+	while (!_terminate_midievent_loop) {
+		_midiin->getMessage(&message);
+
+		if (message.size())
+			_on_midievent(message);
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+}
+
+uint16_t MidiController::num_ports()
+{
+	try {
+		return _midiin->getPortCount();
+	} catch (const RtError& error) {
+		throw MidiException(error.getMessage());
+	}
+}
+
+void MidiController::print_ports()
+{
+	for (unsigned i = 0; i < num_ports(); i++) {
+		auto portName = _midiin->getPortName(i);
+		std::cout << "  Input Port #" << i + 1 << ": " << portName << std::endl;
+	}
 }
